96d8cbdf7c54a04491e831d4b661906075ee0938
Felipe Hernandez <felipe2eu@gmail.com>
2022-11-17 17:05:51 +0100

    Frontend almost done

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,31 +1,29 @@
 class BookmarksController < ApplicationController
-  before_action :find_list, only: %i[new create]
-
   def new
     @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
+
     if @bookmark.save
       redirect_to list_path(@list)
     else
-      render 'lists/new', status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   def destroy
     @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to root_path
+
+    redirect_to list_path(@bookmark.list)
   end
 
   private
-
-  def find_list
-    @list = List.find(params[:list_id])
-  end
 
   def bookmark_params
     params.require(:bookmark).permit(:comment, :movie_id)
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,33 +1,38 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: %i[show destroy]
+
   def index
     @lists = List.all
-  end
-
-  def show
-    @list = List.find(params[:id])
-    @bookmark = Bookmark.new
   end
 
   def new
     @list = List.new
   end
 
+  def show
+  end
+
   def create
     @list = List.new(list_params)
+
     if @list.save
       redirect_to list_path(@list)
     else
-      render :new
+      render :new 
     end
   end
 
   def destroy
-    @list = List.find(params[:id])
     @list.destroy
-    redirect_to root_path
+
+    redirect_to lists_path, notice: 'List was successfully destroyed'
   end
 
   private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
